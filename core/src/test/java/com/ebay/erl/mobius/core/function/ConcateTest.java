d177c2162a8442011d7658980015e1d41b9dbaad
chichiu-at-ebaysf <chichiu@ebaysf.com>
2012-07-02 15:38:26 -0700

    add unit test case for combined values

--- core/src/test/java/com/ebay/erl/mobius/core/function/ConcateTest.java
+++ core/src/test/java/com/ebay/erl/mobius/core/function/ConcateTest.java
@@ -10,7 +10,9 @@ import org.junit.Test;
 
 import com.ebay.erl.mobius.core.MobiusTestJob;
 import com.ebay.erl.mobius.core.builder.Dataset;
+import com.ebay.erl.mobius.core.model.Array;
 import com.ebay.erl.mobius.core.model.Column;
+import com.ebay.erl.mobius.core.model.ResultWrapper;
 import com.ebay.erl.mobius.core.model.Tuple;
 
 public class ConcateTest extends MobiusTestJob 
@@ -49,5 +51,46 @@ public class ConcateTest extends MobiusTestJob
 		
 		String computedResult = func.getComputedResult().toString();
 		assertEquals(trueAnswer.toString(), computedResult);
+	}
+	
+	
+	@Test
+	public void test2()
+		throws IOException
+	{
+		// test values that has wrapped results.
+		List<Tuple> tuples = new ArrayList<Tuple>();
+		
+		Array a1 = new Array();
+		a1.add(1);
+		a1.add(2);
+		a1.add(3);		
+		Tuple t1 = new Tuple();
+		t1.put("C1", new ResultWrapper(a1));		
+		tuples.add(t1);
+		
+		Tuple t2 = new Tuple();
+		t2.put("C1", 4);
+		tuples.add(t2);
+		
+		Array a2 = new Array();
+		a2.add(5);		
+		Tuple t3 = new Tuple();
+		t3.put("C1", a2);
+		tuples.add(t3);
+		
+		
+		Dataset ds = this.createDummyDataset("test", new String[]{"C1"});
+		Concate func = new Concate(new Column(ds, "C1"));
+		func.reset();
+		for( Tuple t:tuples )
+		{
+			func.consume(t);
+		}
+		
+		String expectedResult = "1;2;3;4;5";
+		String actualResult = func.getComputedResult().toString();
+		
+		assertEquals(expectedResult, actualResult);
 	}
 }
